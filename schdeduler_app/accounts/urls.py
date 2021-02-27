b65de2604f73fbd1cbc45dcbeb671d62e8f08f66
evgeniyvojtik <74259947+evgeniyvojtik@users.noreply.github.com>
2021-02-27 22:07:29 +0300

    Delete schdeduler_app directory

--- schdeduler_app/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from accounts.views import UserCreatApiView
-
-urlpatterns = [
-    path('register/',UserCreatApiView.as_view() , name='register'),
-]
--- schdeduler_app/manager/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from manager.models import Event, Country
-
-admin.site.register(Event)
-admin.site.register(Country)
